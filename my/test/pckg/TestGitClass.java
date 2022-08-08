8ee1f2576b986cf2e65c4f580d2e3e785b39c858
Alexander Krause <akr@informatik.uni-kiel.de>
2022-08-08 10:29:18 +0200

    readded TestGitClass

---
+++ my/test/pckg/TestGitClass.java
@@ -0,0 +1,11 @@
+package testgit.my.test.pckg;
+
+public class TestGitClass {
+
+  private final String testVariable;
+
+  public TestGitClass(final String testVariable) {
+    this.testVariable = testVariable;
+  }
+
+}
